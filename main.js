30982e7190982a8ac19e902f5964f8bbb915a057
Mathacevedo <130381642+Mathacevedo@users.noreply.github.com>
2024-03-13 08:43:29 -0300

    e

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+const botoes = document:queryselectorAll("botao");
+for(let i=0;i < botoes.length;i++){
+    botoes.onclick = function(){
 
+        for(let j=0;j<botoes.length;j++){
+            botoes.classlist.remove("ativo");
+        }
+        botoes[i].classlist.add("ativo");
+    }
